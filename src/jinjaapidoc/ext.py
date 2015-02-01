a1d69abf794ee18372d0fac37585b9e02ef42686
David Zuber <zuber.david@gmx.de>
2015-02-01 17:27:05 +0100

    add config to setup and connect to sphinx event

--- src/jinjaapidoc/ext.py
+++ src/jinjaapidoc/ext.py
@@ -2,6 +2,7 @@
 from sphinx.ext import autodoc
 
 import jinjaapidoc
+import jinjaapidoc.updatedoc as updatedoc
 
 
 class ModDocstringDocumenter(autodoc.ModuleDocumenter):
@@ -33,11 +34,31 @@ class ModDocstringDocumenter(autodoc.ModuleDocumenter):
 def setup(app):
     """Setup the sphinx extension
 
+    This will setup autodoc and autosummary.
+    Add the :class:`ModDocstringDocumenter`.
+    Add the config values.
+    Connect builder-inited event to :func:`updatedoc.main`.
+
     :param app: the sphinx app
+    :type app: :class:`sphinx.application.Sphinx`
     :returns: None
     :rtype: None
     :raises: None
     """
     app.setup_extension('sphinx.ext.autodoc')
+    app.setup_extension('sphinx.ext.autosummary')
+
     app.add_autodocumenter(ModDocstringDocumenter)
+
+    app.add_config_value('jinjaapi_outputdir', '', 'env')
+    app.add_config_value('jinjaapi_nodelete', True, 'env')
+    app.add_config_value('jinjaapi_srcdir', '', 'env')
+    app.add_config_value('jinjaapi_exclude_paths', [], 'env')
+    app.add_config_value('jinjaapi_force', True, 'env')
+    app.add_config_value('jinjaapi_followlinks', True, 'env')
+    app.add_config_value('jinjaapi_dryrun', True, 'env')
+    app.add_config_value('jinjaapi_private', True, 'env')
+    app.add_config_value('jinjaapi_templatedirs', [], 'env')
+
+    app.connect('builder-inited', updatedoc.main)
     return {'version': jinjaapidoc.__version__, 'parallel_read_safe': True}

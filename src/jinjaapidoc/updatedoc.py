d6e22bedac664e21ef0c7b55e022a945832182a9
David Zuber <zuber.david@gmx.de>
2015-02-01 17:27:49 +0100

    instead of parsing commandline use sphinx api

--- src/jinjaapidoc/updatedoc.py
+++ src/jinjaapidoc/updatedoc.py
@@ -8,57 +8,17 @@ Usage, just call::
 
 This is automatically executed when building with sphinx. Check the bottom of docs/conf.py.
 """
-import argparse
 import os
-import logging
 import shutil
-import sys
 
 import gendoc
 
 
-log = logging.getLogger(__name__)
-thisdir = os.path.abspath(os.path.dirname(__file__))
-
-
-def setup_argparse():
-    """Sets up the argument parser and returns it
-
-    :returns: the parser
-    :rtype: :class:`argparse.ArgumentParser`
-    :raises: None
-    """
-    log.debug("Setting up argparser.")
-    parser = argparse.ArgumentParser(
-        description="Generate rst files for python source files.\
-        WARNING: this will delete the contents of the output dirs. You can use -nod.")
-    parser.add_argument('src', help='Path to the source directory')
-    parser.add_argument('out', action='store',
-                        help='Directory to place all output')
-    parser.add_argument('exclude_paths', help='Paths to exclude', nargs='*')
-    parser.add_argument('-f', '--force', action='store_true', dest='force',
-                        help='Overwrite existing files')
-    parser.add_argument('-l', '--follow-links', action='store_true',
-                        dest='followlinks', default=False,
-                        help='Follow symbolic links. Powerful when combined '
-                        'with collective.recipe.omelette.')
-    parser.add_argument('-n', '--dry-run', action='store_true', dest='dryrun',
-                        help='Run the script without creating files')
-    parser.add_argument('-P', '--private', action='store_true',
-                        dest='includeprivate',
-                        help='Include "_private" modules')
-    parser.add_argument('-s', '--suffix', action='store', dest='suffix',
-                        help='file suffix (default: rst)', default='rst')
-    parser.add_argument('-nod', '--nodelete', action='store_true',
-                        help='Do not empty the output directories first.')
-    parser.add_argument('-v', '--verbosity', help="Also log debug messages.",
-                        action='store_true')
-    return parser
-
-
-def prepare_dir(directory, delete=True):
+def prepare_dir(app, directory, delete=False):
     """Create apidoc dir, delete contents if delete is True.
 
+    :param app: the sphinx app
+    :type app: :class:`sphinx.application.Sphinx`
     :param directory: the apidoc directory. you can use relative paths here
     :type directory: str
     :param delete: if True, deletes the contents of apidoc. This acts like an override switch.
@@ -67,43 +27,41 @@ def prepare_dir(directory, delete=True):
     :rtype: None
     :raises: None
     """
-    log.debug("Preparing output directories")
+    app.info("Preparing output directories for jinjaapidoc.")
     if os.path.exists(directory):
         if delete:
-            assert directory != thisdir, 'Trying to delete docs! Specify other output dir!'
-            log.debug("Deleting %s", directory)
+            app.debug("Deleting dir %s", directory)
             shutil.rmtree(directory)
-            log.debug("Creating %s", directory)
+            app.debug("Creating dir %s", directory)
             os.mkdir(directory)
     else:
-        log.debug("Creating %s", directory)
+        app.debug("Creating %s", directory)
         os.mkdir(directory)
 
 
-def main(argv=None):
-    """Parse commandline arguments and run the tool
+def main(app):
+    """Parse the config of the app and initiate the generation process
 
-    :param argv: the commandline arguments.
-    :type argv: list
+    :param app: the sphinx app
+    :type app: :class:`sphinx.application.Sphinx`
     :returns: None
     :rtype: None
     :raises: None
     """
-    if argv is None:
-        argv = sys.argv[1:]
-    parser = setup_argparse()
-    args = parser.parse_args(argv)
-    if args.verbosity:
-        l = logging.getLogger()
-        l.setLevel(logging.DEBUG)
-    prepare_dir(args.out, not args.nodelete)
-    gendoc.generate(args.src, args.out,
-                    exclude=args.exclude_paths,
-                    force=args.force,
-                    followlinks = args.followlinks,
-                    dryrun=args.dryrun,
-                    private=args.includeprivate,
-                    suffix=args.suffix)
+    c = app.config
+    src = c.jinjaapi_srcdir
 
-if __name__ == '__main__':
-    main()
+    if not src:
+        return
+
+    out = c.jinjaapi_outputdir or app.env.srcdir
+
+    prepare_dir(app, out, not c.jinjaapi_nodelete)
+    gendoc.generate(app, out, src,
+                    exclude=c.jinjaapi_exclude_paths,
+                    force=c.jinjaapi_force,
+                    followlinks=c.jinjaapi_followlinks,
+                    dryrun=c.jinjaapi_dryrun,
+                    private=c.jinjaapi_includeprivate,
+                    suffix=c.source_suffix,
+                    template_dirs=c.jinjaapi_templatedirs)

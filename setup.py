cfbe08c96e69c92f62b9ddc9d023bbfc5b4189d2
David Zuber <zuber.david@gmx.de>
2015-02-02 02:31:16 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -64,10 +64,15 @@ setup(
     keywords='jinjaapidoc',
     test_suite='jinjaapidoc.test.jinjaapidoc',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Documentation :: Sphinx',
+        'Framework :: Sphinx :: Extension',
     ],
 )
